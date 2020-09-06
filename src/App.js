729eacf7d3b6e19ddc4c0b473304e5bb0eb27cd1
Anass Bendarsi <bendarsi@gmail.com>
2020-09-06 21:13:13 +0100

    chore: navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import styled from 'styled-components'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Navbar from './components/layouts/Navbar';
+import Home from './components/page/Home';
+import CreateEntity from './components/entity/CreateEntity';
 
 function App() {
   return (
-    <div>
-      hi
-    </div>
+    <Router>
+      <Container>
+        <Navbar/>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/create-entity">
+            <CreateEntity></CreateEntity>
+          </Route>
+        </Switch>
+      </Container>
+    </Router>
   );
 }
+
+const Container = styled.div`
+  max-width: 1500px;
+  margin: 0 auto;
+`;
 
 export default App;
---
+++ src/components/layouts/Navbar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <Nav>
+      <Brand to="/">brand</Brand>
+      <Links>
+        <Li to="/singup">SignUp</Li>
+        <Li to="/login">LogIn</Li>
+        <Li to="/profile">Profile</Li>
+        <Li to="/LogOut">LogOut</Li>
+      </Links>
+    </Nav>
+  )
+}
+
+const Nav = styled.div`
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between; 
+  border-bottom: 2px solid #eee;
+`;
+
+const Brand = styled(Link)` 
+  font-size: 2rem;
+  cursor: pointer;
+  text-decoration: none;
+  color: black;
+`;
+
+const Links = styled.ul`
+  display: flex;
+  padding: 1rem;
+  list-style: none;
+`;
+
+const Li = styled(Link)`
+  background-color: white;
+  margin-left: 2rem;
+  text-decoration: none;
+  color: black;
+  font-size: 1.2rem;
+  &:hover {
+    color: #aaa;
+  }
+`;
+
+
