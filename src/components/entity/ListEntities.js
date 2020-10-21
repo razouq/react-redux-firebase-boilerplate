794bade057efcf7a6c972e2847c1667c88c33268
Anass Bendarsi <bendarsi@gmail.com>
2020-10-21 16:10:19 +0100

    chore(list entities): design table and edit delete buttons

--- src/components/entity/ListEntities.js
+++ src/components/entity/ListEntities.js
@@ -12,12 +12,28 @@ const ListEntities = () => {
     return <p>Loading...</p>;
   }
   const entitiesList = Object.keys(entities).map((key) => (
-    <p key={key}>{entities[key].name}</p>
+    <Tr key={key}>
+      <Td>{entities[key].name}</Td>
+      <Td>{entities[key].description}</Td>
+      <Td>
+        <EditButton href="#">edit</EditButton>
+        <DeleteButton href="#">delete</DeleteButton>
+      </Td>
+    </Tr>
   ));
   return (
     <>
       <H1>List Of My Entities</H1>
-      {entitiesList}
+      <Table>
+        <thead>
+          <Tr>
+            <Th>Name</Th>
+            <Th>Description</Th>
+            <Th>Actions</Th>
+          </Tr>
+        </thead>
+        <tbody>{entitiesList}</tbody>
+      </Table>
     </>
   );
 };
@@ -26,4 +42,60 @@ export default ListEntities;
 
 const H1 = styled.h1`
   margin: 2.5rem;
+`;
+
+const Table = styled.table`
+  /* border: 0.2rem solid black; */
+  /* padding: 1rem; */
+  border-spacing: 0;
+  width: 80%;
+  margin: 0 auto;
+  border: 1px solid #ddd;
+  border-collapse: collapse;
+`;
+
+const Tr = styled.tr`
+  padding: 0.5rem;
+`;
+
+const Th = styled.th`
+  padding: 0.5rem;
+  border-collapse: collapse;
+  border: 1px solid #ddd;
+`;
+
+const Td = styled.td`
+  padding: 0.5rem;
+  text-align: center;
+  border: 1px solid #ddd;
+`;
+
+const EditButton = styled.a`
+  display: inline-block;
+  padding: 0.5rem 1.5rem;
+  margin: 0.5rem;
+  text-decoration: none;
+  color: #3185fc;
+  border-bottom: 0.1rem solid #3185fc;
+  border-radius: 2px;
+  transition: all 0.3s;
+  &:hover {
+    background-color: #3185fc;
+    color: white;
+  }
+`;
+
+const DeleteButton = styled.a`
+  display: inline-block;
+  padding: 0.5rem 1.5rem;
+  margin: 0.5rem;
+  text-decoration: none;
+  color: #e4113f;
+  border-bottom: 0.1rem solid #e4113f;
+  border-radius: 2px;
+  transition: all 0.3s;
+  &:hover {
+    background-color: #e4113f;
+    color: white;
+  }
 `;
