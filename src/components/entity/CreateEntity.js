67b7d3fda52b85bcf36f739804be490c54bdc84d
anass bendarsi <bendarsi@gmail.com>
2021-03-31 23:39:37 +0100

    create entity validation, integration of yup library

--- src/components/entity/CreateEntity.js
+++ src/components/entity/CreateEntity.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import { createEntity } from '@store/actions/entityActions';
 import {
   Button,
@@ -13,8 +15,18 @@ import {
   Textarea,
 } from '../../elements/entity';
 
+const schema = yup.object().shape({
+  name: yup.string().test({
+    name: 'name',
+    message: 'must have at least 2 chars',
+    test: (value) => value.length >= 2,
+  }),
+});
+
 export default function CreateEntity() {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, errors } = useForm({
+    resolver: yupResolver(schema),
+  });
   const history = useHistory();
   const dispatch = useDispatch();
 
@@ -22,6 +34,7 @@ export default function CreateEntity() {
   const [description, setDescription] = useState('');
 
   const onSubmit = (data) => {
+    console.log(errors);
     try {
       dispatch(createEntity(data));
       history.push('/list-entities');
@@ -29,7 +42,7 @@ export default function CreateEntity() {
       console.error(e);
     }
   };
-
+  console.log(errors);
   return (
     <div>
       <H1>Create Entity</H1>
@@ -43,8 +56,11 @@ export default function CreateEntity() {
             id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            ref={register}
+            ref={register()}
           />
+
+          {errors.name && <span>2 char is the minimum</span>}
+          {errors.name && <span>2 char is the minimum</span>}
         </Field>
         <Field>
           <Label htmlFor="description">description</Label>
